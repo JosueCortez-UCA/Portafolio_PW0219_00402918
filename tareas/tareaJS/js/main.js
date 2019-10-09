98618aa7285e5472d73cd9afcf4d3ae8de11ee84
JosueCortez-UCA <00402918@uca.edu.sv>
2019-10-09 13:47:05 -0600

    ADD: js terminado y funcionando, ADD: id a tbody en main

--- tareas/tareaJS/js/main.js
+++ tareas/tareaJS/js/main.js
@@ -1,14 +1,18 @@
-let journal = [];
+let diario = [];
 let eventos = [];
 
 // Funcion para agregar eventos de un dia
+document.getElementById("btnEvento").onclick = agregarEvento;
+
 function agregarEvento(){
-    let unEvento = document.getElementById("evento").value;
-    eventos.push(unEvento);
+    let unEvento = document.getElementById("evento");
+    eventos.push(unEvento.value);
+    unEvento.value = "";
 }
 
 // Funcion para agregar el dia al journal
-function addEntry() {
+document.getElementById("btnSubmit").onclick = agregarDia;
+function agregarDia() {
     let accidente;
     let radioBTNS = document.getElementsByName('accident');
 
@@ -18,5 +22,21 @@ function addEntry() {
         }
     } 
 
-    journal.push({eventos, accidente});
+    diario.push({eventos, accidente});
+    agregarFila(eventos, accidente);
+    eventos = [];
 }
+
+// Funciona para agregar una fila en el HTML
+let agregarFila = (evento, accidente) => {
+    let tabla = document.getElementById('bitacoraBody');
+    let fila = document.createElement("tr");
+
+    fila.className = "table_active";
+    fila.innerHTML = `
+          <td class="bitacora-event"> ${evento} </td>
+          <td class="bitacora-accedent"> ${accidente} </td>
+      `;
+  
+    tabla.appendChild(fila);
+};
