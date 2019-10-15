2ff21340886f2fee627a53a1efee8cb5041f69aa
JosueCortez-UCA <00402918@uca.edu.sv>
2019-10-15 11:05:30 -0600

    ADD: codigo necesario para hacer que solo se muestre un articulo a la vez, y la ultima linea de codigo para mostrar al cargar la pagina el primer articulo; FIX: nombre de variable

--- tareas/tareaJS/ejercicio_3/js/main.js
+++ tareas/tareaJS/ejercicio_3/js/main.js
@@ -1,11 +1,22 @@
 let dias = document.querySelectorAll(".dias");
+let bandera = false;
+let ultima_seleccion;
 
 for (let index = 0; index < dias.length; index++) {
     dias[index].addEventListener("click", () => {
-        let dia = dias[index].getAttribute("href").split('#');
-        let id_dia = dia[dia.length-1];
+        let dia_ruta = dias[index].getAttribute("href").split('#');
+        let dia_id = dia_ruta[dia_ruta.length-1];
         
-        let dia_seleccionado = document.getElementById(id_dia);
+        let dia_seleccionado = document.getElementById(dia_id);
         dia_seleccionado.classList.toggle("hide");
+
+        if (bandera) {
+            ultima_seleccion.classList.toggle("hide");
+        }
+        
+        ultima_seleccion = dia_seleccionado;
+        bandera = true;
     });
 };
+
+document.getElementById("day_1").onclick = dias[0].click();
