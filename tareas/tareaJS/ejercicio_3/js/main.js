a304896695ae592b07563ba8316d91fde3a2b1ff
JosueCortez-UCA <00402918@uca.edu.sv>
2019-10-15 09:53:16 -0600

    ADD,FIX: nuevo codigo js para hacer funcionar la pagina

--- tareas/tareaJS/ejercicio_3/js/main.js
+++ tareas/tareaJS/ejercicio_3/js/main.js
@@ -1,38 +1,11 @@
-let dias = document.getElementsByClassName("dias");
-let bandera = false;
-let ultimaSeleccion;
+let dias = document.querySelectorAll(".dias");
 
-function mostrarDia(dia){
-    let articulo = document.getElementById(dia);
-    articulo.classList.toggle("hide");
-
-    if (bandera) {
-        let ultimoArticulo = document.getElementById(ultimaSeleccion);
-        ultimoArticulo.classList.toggle("hide");
-    }
-
-    ultimaSeleccion = dia;
-    bandera = true;
-
-    return false;
-};
-/*
-document.getElementById("link-day22").onclick = mostrarDia("day-2");
-document.getElementById("link-day11").onclick = mostrarDia("day-1");
-*/
-/*
-document.getElementById("link-day11").addEventListener("click", mostrarDia("day-1"), false);
-document.getElementById("link-day22").addEventListener("click", mostrarDia("day-2"), false);
-*/
-/*
 for (let index = 0; index < dias.length; index++) {
-    dias[index].addEventListener("click", mostrarDia("day-1"));
+    dias[index].addEventListener("click", () => {
+        let dia = dias[index].getAttribute("href").split('#');
+        let id_dia = dia[dia.length-1];
+        
+        let dia_seleccionado = document.getElementById(id_dia);
+        dia_seleccionado.classList.toggle("hide");
+    });
 };
-*/
-
-/*
-for (let index = 0; index < dias.length; index++) {
-    x = document.getElementById("link-day1");
-    console.log(x);
-}
-*/
