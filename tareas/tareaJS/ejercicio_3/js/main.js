61e403afacef9d3d8b3aea211a4ede83bcab9520
JosueCortez-UCA <00402918@uca.edu.sv>
2019-10-15 12:43:14 -0600

    ADD: codigo base de ejemplo para video

--- tareas/tareaJS/ejercicio_3/js/main.js
+++ tareas/tareaJS/ejercicio_3/js/main.js
@@ -20,3 +20,22 @@ for (let index = 0; index < dias.length; index++) {
 };
 
 document.getElementById("day_1").onclick = dias[0].click();
+
+/* CODIGO BASE */
+/*
+let dias = document.querySelectorAll(".dias");
+
+for (let index = 0; index < dias.length; index++) {
+    dias[index].addEventListener("click", () => {
+        let dia_ruta = dias[index].getAttribute("href").split('#');
+        let dia_id = dia_ruta[dia_ruta.length-1];
+        
+        let dia_seleccionado = document.getElementById(dia_id);
+        dia_seleccionado.classList.toggle("hide");
+    });
+};
+*/
+
+/* EXTRA
+console.log(dias);
+*/
